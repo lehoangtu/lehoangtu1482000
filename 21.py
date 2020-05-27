83557867177aa5b308f964598c658cc4279173c1
lehoangtu <61909591+lehoangtu@users.noreply.github.com>
2020-05-27 21:07:21 +0700

    21.py

---
+++ 21.py
@@ -0,0 +1,7 @@
+giá trị  = []
+mục = [ x  cho  x  trong  đầu vào ( "Lời đề nghị của bạn:" ). chia ( ',' )]
+cho  p  trong  các mục :
+    intp  =  int ( p , 2 )
+    nếu  không  intp % 5 :
+        giá trị . phụ lục ( p )
+in ( ',' . tham gia ( giá trị ))
