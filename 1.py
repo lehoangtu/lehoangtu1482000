bb4234581781400434ff3329b654e94360687232
lehoangtu <61909591+lehoangtu@users.noreply.github.com>
2020-05-27 21:01:58 +0700

    1.py

---
+++ 1.py
@@ -0,0 +1,17 @@
+rùa nhập khẩu
+cửa sổ = rùa . Màn hình ()
+cửa sổ . bgcolor ( "ánh sáng xanh" )
+
+họa sĩ = rùa . Rùa ()
+họa sĩ . fillcolor ( 'xanh' )
+họa sĩ . pencolor ( 'màu xanh' )
+họa sĩ . penize ( 3 )
+
+def  drawsq ( t , s ):
+    cho  tôi  trong  phạm vi ( 4 ):
+        t . về phía trước ( s )
+        t . trái ( 90 )
+
+cho  tôi  trong  phạm vi ( 1 , 180 ):
+    họa sĩ . trái ( 18 )
+    drawsq ( họa sĩ , 200 )
