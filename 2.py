3b233fd25064cba0ed87987f72799071dc5954d7
lehoangtu <61909591+lehoangtu@users.noreply.github.com>
2020-05-27 21:06:01 +0700

    2.py

---
+++ 2.py
@@ -0,0 +1,11 @@
+rùa nhập khẩu , ngẫu nhiên
+màu sắc = [ "đỏ" , "xanh" , "xanh" , "cam" , "tím" , "hồng" , "vàng" ]
+họa sĩ = rùa . Rùa ()
+họa sĩ . penize ( 3 )
+cho  tôi  trong  phạm vi ( 10 ):
+    màu = ngẫu nhiên . sự lựa chọn ( màu sắc )
+    họa sĩ . pencolor ( màu )
+    họa sĩ . vòng tròn ( 100 )
+    họa sĩ . phải ( 30 )
+    họa sĩ . trái ( 60 )
+    họa sĩ . đặt ra ( 0 , 0 )
