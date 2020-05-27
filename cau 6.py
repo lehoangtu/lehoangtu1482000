bb28c32744cc5a33bb9d99712351592b04f18468
lehoangtu <61909591+lehoangtu@users.noreply.github.com>
2020-05-27 21:14:49 +0700

    cau 6.py

---
+++ cau 6.py
@@ -0,0 +1,8 @@
+def  get_sum ( * num ):
+    tmp = 0
+    #duyet cac tham vậy
+    cho  tôi  bằng  num :
+        tmp + = i
+    trở lại  tmp
+kết quả = get_sum ( 1 , 2 , 3 , 4 , 5 )
+in ( kết quả )
